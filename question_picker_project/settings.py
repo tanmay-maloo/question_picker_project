54d72520afd5370a2c6c77ab9c9e72162dc60453
tanmay maloo <tanmaymaloo@tanmays-MacBook-Pro.local>
2025-02-02 22:32:45 +0530

    tanamy fix

--- question_picker_project/settings.py
+++ question_picker_project/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'accounts/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 LOGIN_URL = 'login'
