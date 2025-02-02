85ecedb6183ec3485cd063e5ee57b17ad514a82b
tanmay maloo <tanmaymaloo@tanmays-MacBook-Pro.local>
2025-02-02 22:18:06 +0530

    fix

--- question_picker_project/settings.py
+++ question_picker_project/settings.py
@@ -118,9 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'accounts/static'),
 ]
 
 LOGIN_URL = 'login'
